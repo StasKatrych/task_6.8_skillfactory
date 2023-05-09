9b6e81d204e0bfc94b833593beb0df30af9ee83f
StasKatrych <stas_katrych@mail.ru>
2023-05-09 13:36:16 +0700

    added project

---
+++ task2/script.js
@@ -0,0 +1,14 @@
+document.querySelector('#consoleLog').addEventListener('click',
+(event) => {
+    alert('Метод для вывода сообщений в Веб-консоль.');
+})
+
+document.querySelector('#alert').addEventListener('click',
+(event) => {
+    alert('Функция alert позволяет вывести какую-либо информацию во всплывающем окне в браузере.');
+})
+
+document.querySelector('#prompt').addEventListener('click',
+(event) => {
+    alert('Функция prompt выводит окно для ввода данных. ');
+})
